6e14177ee7d64e0f5308ad9882d874a307f9d059
FarhadJs <farhadfallahi2004@gmail.com>
2024-06-23 16:33:44 +0330

    init: set first changes

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+  import { ref } from "vue";
+  import HightLightPink from "./assets/svg/bg-highlight.svg";
+  import CupCakes from "./components/CupCakes.vue";
+
+  const theme = ref("dark");
+
+  const ChangeTheme = (e) => {
+    console.log(theme);
+    if (!e.target.checked) theme.value = "light";
+    else theme.value = "dark";
+    document.documentElement.className = theme.value;
+  };
+</script>
+
+<template>
+  <div class="overflow-hidden w-full">
+    <h1 class="text-4xl font-bold dark:text-white">Hello World!</h1>
+    <input type="checkbox" :onclick="ChangeTheme" />
+    <img :src="HightLightPink" class="pink-highlight w-[100vh] " alt="" />
+
+    <CupCakes />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/CupCakes.vue
@@ -0,0 +1,18 @@
+<template>
+  <div dir="rtl">
+    <img
+      class="w-[300px] translate-x-40 -translate-y-16"
+      src="../assets//img/caramel-cupcake.png"
+      alt="caramel-cupcake" />
+    <img
+      class="w-[400px] -translate-x-96 -translate-y-10"
+      src="../assets//img/pink-cupcake.png"
+      alt="caramel-cupcake" />
+    <img
+      class="w-[300px] translate-x-40 -translate-y-20"
+      src="../assets//img/chocolate-cupcake.png"
+      alt="caramel-cupcake" />
+  </div>
+</template>
+<script setup></script>
+<style></style>
